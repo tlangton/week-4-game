40458a0003ec3afd67e57e7567f8a8b8934e953f
Tom Langton <tom@pulpinfo.com>
2017-02-07 11:38:43 -0500

    first working logic

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,19 @@
-var gameStats = {
-	crystalOne:0,
-	crystalTwo:0
+var game = {
+	wins:0,
+	losses:0
 }
 
 
+var round = {
+	crystal1:10,
+	crystal2:2,
+	crystal3:3,
+	crystal4:17,
+	target:0,
+	score:0
+}
+
+
+var butID;
+
+ var roger=3;
